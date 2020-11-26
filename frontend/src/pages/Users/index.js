5658e74f72ac41fd42baa132f9aa7d2a9ca7e0b7
camposyuri <yurinapoleao@gmail.com>
2020-11-26 20:53:45 -0300

    [ADD] - Listing users in table

--- frontend/src/pages/Users/index.js
+++ frontend/src/pages/Users/index.js
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import TableList from "../../components/Table";
+import TableBody from "../../components/Table/TableBody";
+import TableHeader from "../../components/Table/TableHeader";
 import api from "../../services/api";
 
 const Users = () => {
@@ -12,13 +16,34 @@ const Users = () => {
 
   return (
     <>
-      {data.map((item) => {
-        return (
-          <h1>
-            {item.id} - {item.username}
-          </h1>
-        );
-      })}
+      <h1 className="my-5">Listing of users</h1>
+
+      <TableList>
+        <TableHeader
+          id="ID"
+          name="Name"
+          usename="UserName"
+          email="E-mail"
+          number="Phone"
+          action="Actions"
+        />
+        {data.map((item) => {
+          return (
+            <TableBody
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              username={item.username}
+              email={item.email}
+              number={item.number}
+            >
+              <Link className="btn btn-warning" to={"/edit/" + item.id}>
+                Edit
+              </Link>
+            </TableBody>
+          );
+        })}
+      </TableList>
     </>
   );
 };
